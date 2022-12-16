ef1aac88721a9c582cfd1df18f1d9acbe9832e99
no_to_last <77478360+futre-re@users.noreply.github.com>
2022-12-16 21:06:44 +0800

    Add files via upload

--- ni_shuo_de_du_dui/IndexOperate/src/lib.rs
+++ ni_shuo_de_du_dui/IndexOperate/src/lib.rs
@@ -1,48 +1,47 @@
 use dashmap::DashMap;
 use std::any::type_name;
 use std::collections::BTreeMap;
+use std::ops::Bound::Included;
 use std::vec;
 //#[warn(unreachable_code)]
-fn get_usize<K>(any_name: K) -> usize {
-    let addr = &any_name as *const K as usize;
-    addr
-}
-fn usize_get<K>(addr: usize) -> *mut K {
-    let pa = addr as *mut K;
-    pa
-}
 struct Basedata {
     tree: DashMap<String, usize>,
     //pool: ThreadPool,
 }
 impl Basedata {
     pub fn new() -> Basedata {
+        let data = DashMap::new();
         Basedata {
-            tree: DashMap::new(),
+            tree: data,
             //pool: ThreadPool::new(10),
             //listener: mpsc::channel(),
         }
     }
+    fn get_usize<'a, K>(any_name: &K) -> usize {
+        let addr = any_name as *const K as usize;
+        addr
+    }
+    fn usize_get<'a, K>(addr: usize) -> *mut K {
+        let pa = addr as *mut K;
+        pa
+    }
     //fn new_chanel<K, V>() {}
-    fn new_tree<K: Ord, V>(&self) -> Option<bool> {
+    fn new_tree<'a, K: Ord, V>(&self) -> BTreeMap<K, V> {
         let root_name = format!("{},{}", type_name::<Option<K>>(), type_name::<Option<V>>());
-        let root_temp = self.contains::<K, V>(&root_name);
-        if root_temp.is_some() {
-            return Some(false);
-        } else {
-            let new_tree = BTreeMap::<K, V>::new();
-            self.tree.insert(root_name, get_usize(new_tree));
+        let new_tree = BTreeMap::<K, V>::new();
 
-            return Some(true);
-        }
-        //None
+        //let root_n = format!("{},{}", type_name::<Option<K>>(), type_name::<Option<V>>());
+        self.tree.insert(root_name, Self::get_usize(&new_tree));
+        new_tree
     }
     fn contains<K: Ord, V>(&self, root_name: &String) -> Option<&mut BTreeMap<K, V>> {
         //let root_name = format!("{},{}", type_name::<Option<K>>(), type_name::<Option<V>>());
         if self.tree.contains_key(root_name) {
             let root_addr = *self.tree.get(root_name).unwrap();
             unsafe {
-                let root = usize_get::<BTreeMap<K, V>>(root_addr).as_mut().unwrap();
+                let root = Self::usize_get::<BTreeMap<K, V>>(root_addr)
+                    .as_mut()
+                    .unwrap();
                 return Some(root);
             }
         }
@@ -69,7 +68,7 @@ trait IndexOperate<K: Ord, V: Copy> {
     fn delete(&self, key: &K, range_end: &K) -> Vec<V>;
     fn insert_or_update(&self, key: K, value: V) -> Option<V>;
 }
-impl<K: Ord, V: Copy> IndexOperate<K, V> for Basedata {
+impl<'a, K: Ord + 'static + Copy, V: Copy> IndexOperate<K, V> for Basedata {
     fn get(&self, key: &K, range_end: &K) -> Vec<&V> {
         let root_name = format!("{},{}", type_name::<Option<K>>(), type_name::<Option<V>>());
         let root_temp = self.contains::<K, V>(&root_name);
@@ -79,6 +78,7 @@ impl<K: Ord, V: Copy> IndexOperate<K, V> for Basedata {
             for (_, v) in root.range(key..range_end) {
                 ret_vec.push(v);
             }
+            return ret_vec;
         }
         vec![]
     }
@@ -89,10 +89,11 @@ impl<K: Ord, V: Copy> IndexOperate<K, V> for Basedata {
         if root_temp.is_some() {
             let root = root_temp.unwrap();
             let mut ret_vec = Vec::new();
-            for (_, val) in root.range(key..range_end) {
-                ret_vec.push(*val);
+            for (_, val) in root.range((Included(key), Included(range_end))) {
+                let temp = val.clone();
+                ret_vec.push(temp);
             }
-            root.retain(|k, _| *k < *key && *k > *range_end);
+            root.retain(|k, _| k < key && k > range_end);
             return ret_vec;
         }
         vec![]
@@ -103,15 +104,22 @@ impl<K: Ord, V: Copy> IndexOperate<K, V> for Basedata {
         let root_temp = self.contains::<K, V>(&root_name);
         if root_temp.is_some() {
             let root = root_temp.unwrap();
-            return root.insert(key, value);
+            let ret = root.insert(key, value);
+            //println!("{}", root.len());
+            return ret;
         } else {
-            let flag_task = self.new_tree::<K, V>();
-            if flag_task == Some(true) {
-                let root_temp = self.contains::<K, V>(&root_name);
-                let root = root_temp.unwrap();
-                return root.insert(key, value);
+            self.new_tree::<K, V>();
+            let root_addr = *self.tree.get(&root_name).unwrap();
+            unsafe {
+                let root = Self::usize_get::<BTreeMap<K, V>>(root_addr)
+                    .as_mut()
+                    .unwrap();
+                let ret = root.insert(key, value);
+                //println!("{}", root.len());
+                return ret;
             }
+            //println!("{}", root.len());
+            //println!("{}", root.len());
         }
-        None
     }
 }
